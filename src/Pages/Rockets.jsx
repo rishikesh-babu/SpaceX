d042d84f42999a8d8e37905598428588b38cde64
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-04 15:23:33 +0530

    Udated rocket page

--- src/Pages/Rockets.jsx
+++ src/Pages/Rockets.jsx
@@ -21,29 +21,29 @@ export default function Rockets() {
     }
 
     return (
-        <div className="py-6 px-1 min-h-screen">
+        <div className="py-6 px-1 sm:px-3 md:px-4 lg:px-6 min-h-screen">
             <h1 className="text-3xl font-bold mb-6 text-center">🚀 SpaceX Rockets</h1>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {rockets.map((rocket) => (
-          <div
-            key={rocket.id}
-            className="p-6 bg-gray-200 dark:bg-gray-900 rounded-xl shadow-md"
-          >
-            {/* Rocket Image (clickable) */}
-            <Link to={`/rockets/${rocket.id}`}>
-              <img
-                src={rocket.flickr_images[0]}
-                alt={rocket.name}
-                className="rounded-lg w-full h-56 object-cover mb-4 hover:scale-105 transition-transform cursor-pointer"
-              />
-            </Link>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {rockets.map((rocket) => (
+                    <div
+                        key={rocket.id}
+                        className="p-6 bg-gray-200 dark:bg-gray-900 rounded-xl shadow-md"
+                    >
+                        {/* Rocket Image (clickable) */}
+                        <Link to={`/rockets/${rocket.id}`}>
+                            <img
+                                src={rocket.flickr_images[0]}
+                                alt={rocket.name}
+                                className="rounded-lg w-full h-56 object-cover mb-4 hover:scale-105 transition-transform cursor-pointer"
+                            />
+                        </Link>
 
-            {/* Rocket Name */}
-            <h2 className="text-2xl font-bold mb-2">{rocket.name}</h2>
-          </div>
-        ))}
-      </div>
+                        {/* Rocket Name */}
+                        <h2 className="font-bold text-2xl text-center">{rocket.name}</h2>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
